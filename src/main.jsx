b10f71cb3622f55a11db6e35ca626ac9254e1ea3
BOCBuilderofCharacter <tannerg2004@outlook.com>
2025-10-13 14:28:20 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import StickerFinderLanding from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <StickerFinderLanding />
+  </React.StrictMode>
+);
